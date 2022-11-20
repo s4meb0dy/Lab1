274f6eab996c10952f8b1ce2ce9e04ebdf33d8bf
Nikita Shepel <shepel.nkt@gmail.com>
2022-11-20 21:18:46 +0200

    add utils and complete server by adding content types

--- src/server.js
+++ src/server.js
@@ -2,13 +2,40 @@ import * as http from 'node:http'
 import router from './router.js'
 import defaultHandler from './defaultHandler.js'
 import helpers from './helpers.js'
+import { safeJSON } from './utils.js'
+
+const processedContentTypes = {
+  'text/html': (text) => text,
+  'text/plain': (text) => text,
+  'application/json': (json) => safeJSON(json, {}),
+  'application/x-www-form-urlencoded': (data) => {
+    return Object.fromEntries(new URLSearchParams(data))
+  },
+}
 
 const server = http.createServer(async (req, res) => {
   const url = new URL(req.url || '/', `https://${req.headers.host}`)
   const routeModule = router.get(url.pathname) ?? {}
   const handler = routeModule[req?.method] ?? defaultHandler
 
-  handler(req, Object.assign(res, helpers), url)
+  let payload = {}
+  let rawRequest = ''
+  for await (const chunk of req) {
+    rawRequest += chunk
+  }
+  if (req.headers['content-type']) {
+    const contentType = req.headers['content-type'].split(';')[0]
+    if (processedContentTypes[contentType]) {
+      payload = processedContentTypes[contentType](rawRequest)
+    }
+  }
+
+  try {
+    handler(req, Object.assign(res, helpers), url, payload, rawRequest)
+  } catch (e) {
+    res.statusCode = 500
+    res.end(process.env.NODE_ENV === 'production' ? 'internal error' : e)
+  }
 })
 server.on('clientError', (err, socket) => {
   if (err) console.log(err)
