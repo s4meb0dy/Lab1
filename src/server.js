28581aa7c7236bbff56caca46b047c2d9f74c026
Nikita Shepel <shepel.nkt@gmail.com>
2022-11-20 20:55:25 +0200

    fix error handling and run npm lint

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
-import * as http from "node:http";
-import router from "./router.js";
-import defaultHandler from "./defaultHandler.js";
+import * as http from 'node:http'
+import router from './router.js'
+import defaultHandler from './defaultHandler.js'
 
 const server = http.createServer(async (req, res) => {
-  const url = new URL(req.url || "/", `https://${req.headers.host}`);
-  const routeModule = router.get(url.pathname) ?? {};
-  const handler = routeModule[req?.method] ?? defaultHandler;
+  const url = new URL(req.url || '/', `https://${req.headers.host}`)
+  const routeModule = router.get(url.pathname) ?? {}
+  const handler = routeModule[req?.method] ?? defaultHandler
 
-  handler(req, res);
-});
-server.on("clientError", (err, socket) => {
-socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
-});
-server.listen(parseInt(process.env.PORT) || 8000);
+  handler(req, res)
+})
+server.on('clientError', (err, socket) => {
+  if (err) console.log(err)
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+})
+server.listen(parseInt(process.env.PORT) || 8000)
 
-process.on("SIGINT", () => {
+process.on('SIGINT', () => {
   server.close((error) => {
     if (error) {
-      console.error(error);
-      process.exit(1);
+      console.error(error)
+      process.exit(1)
     }
-  });
-});
+  })
+})
