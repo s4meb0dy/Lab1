4d320b36ea5b761b3ea04fcfa76ae1f207427a52
Anton Bodnar <antonbond2004@gmail.com>
2022-11-22 19:50:38 +0200

    edit index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,8 @@ function OPTIONS(req, res) {
   res.json({ name: 'root options' })
 }
 
-function POST(req, res) {
-  res.json({ text: 'root post' })
+function POST(req, res, url, payload) {
+    res.json({ contentType: req.headers['content-type'], payload })
 }
 
 export { GET, OPTIONS, POST }
